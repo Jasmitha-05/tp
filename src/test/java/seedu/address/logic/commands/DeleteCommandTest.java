91dbc609e0b268debfabedd3fd39d9448c337022
KyiCode <kaijunc23@gmail.com>
2026-03-16 01:03:46 +0800

    increment DeleteCommandTest

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -3,21 +3,23 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.BOB;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Application;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Role;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -75,6 +77,31 @@ public class DeleteCommandTest {
         assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
 
         DeleteCommand deleteCommand = new DeleteCommand(outOfBoundIndex);
+
+        assertCommandFailure(deleteCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    // Non indexed Deletion Testcases
+    @Test
+    public void execute_validNormalDeleteCommand_success() {
+        Application deleteTarget = BOB;
+        Model model2 = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        model2.addPerson(BOB);
+
+        DeleteCommand deleteCommand = new DeleteCommand(deleteTarget.getName(), deleteTarget.getRole());
+
+        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS,
+                Messages.format(deleteTarget));
+
+        ModelManager expectedModel = new ModelManager(model2.getAddressBook(), new UserPrefs());
+        expectedModel.deletePerson(deleteTarget);
+
+        assertCommandSuccess(deleteCommand, model2, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_NormalDeleteCommandNoTarget_throwsCommandException() {
+        DeleteCommand deleteCommand = new DeleteCommand(new Name("NonExistentName"), new Role("Engineer"));
 
         assertCommandFailure(deleteCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
